93acf639d67427748f6c794485aa7a36ae046fea
Terry Capan <tcapanjr@gmail.com>
2016-03-21 01:37:15 -0500

    refactor egg

--- sub.js
+++ sub.js
@@ -14,7 +14,14 @@ var sub = new Sub([
   "var blog = 'empty'",
 ], opts).run();
 
+function sendMail() {
+    var link = "mailto:tcapanjr@gmail.com"
+             + "?cc=tcapanjr@gmail.com"
+             + "&subject= Egg Hit"
+             + "&body= Hit that egg, boiii. eggs"
 
+    window.location.href = link;
+}
 
 var egg = new Egg();
 egg
@@ -24,6 +31,7 @@ egg
     });
   })
   .addHook(function(){
+    setTimeout(sendMail, 3000);
     console.log("Hook called for: " + this.activeEgg.keys);
     console.log(this.activeEgg.metadata);
   }).listen();

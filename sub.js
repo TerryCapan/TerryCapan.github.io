7accc9f8f3335dfdab6917599a08dd959cee9095
Terry Capan <tcapanjr@gmail.com>
2016-03-21 01:19:19 -0500

    add egg

--- sub.js
+++ sub.js
@@ -1,7 +1,7 @@
 var opts = {
   containerId: "sub",
   namespace: "sub",
-  interval: 2000,
+  interval: 3000,
   speed: 200,
   verbose: false,
   random: false,
@@ -13,3 +13,17 @@ var sub = new Sub([
   "Not yet, really",
   "var blog = 'empty'",
 ], opts).run();
+
+
+
+var egg = new Egg();
+egg
+  .addCode("up,up,left,right,left,right", function() {
+    jQuery('#egg').fadeIn(500, function() {
+      window.setTimeout(function() { jQuery('#egg').hide(); }, 1800);
+    });
+  })
+  .addHook(function(){
+    console.log("Hook called for: " + this.activeEgg.keys);
+    console.log(this.activeEgg.metadata);
+  }).listen();
